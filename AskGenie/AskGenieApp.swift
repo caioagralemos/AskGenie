ef312eb666eb66682825704d8642e85819d38a97
Caio <caioagralemos@live.com>
2024-07-28 00:13:20 -0300

    Initial Commit

---
+++ AskGenie/AskGenieApp.swift
@@ -0,0 +1,17 @@
+//
+//  AskGenieApp.swift
+//  AskGenie
+//
+//  Created by Caio on 28/07/24.
+//
+
+import SwiftUI
+
+@main
+struct AskGenieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
